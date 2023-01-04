89efc0f1ce939720e2cba5d391c2e2ff5099baf4
miguel5219 <5219@holbertonstudents.com>
2023-01-04 13:15:24 -0500

    .

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -16,9 +16,9 @@ $(function () {
 
     $.get('http://0.0.0.0:5001/api/v1/status/', function(res) {
         if (res.status === 'OK') {
-            $('div#api_status').addClass('available');
+            $('DIV#api_status').addClass('available');
         } else {
-            $('div#api_status').removeClass('available');
+            $('DIV#api_status').removeClass('available');
         }
     });
 });
