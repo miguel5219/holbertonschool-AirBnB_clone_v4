9a7f7a5f358f4b3fbb73c42d5434f28c263fa221
miguel5219 <5219@holbertonstudents.com>
2023-01-05 15:42:44 -0500

    change request

---
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -0,0 +1,22 @@
+$(function () {
+    function ToArray (amenities) {
+        $('.amenities H4').text(Object.values(amenities).join(', '));
+    }
+
+    const allAmenities = {};
+    $('.amenities input').on('change', function () {
+        if ($(this).is(':checked')) {
+            allAmenities[$(this).attr('data-id')] = $(this).attr('data-name');
+        } else {
+            delete allAmenities[$(this).attr('data-id')];
+        }
+        ToArray(allAmenities);
+    });
+    $.get('http://localhost:5001/api/v1/places_search/', function(data) {
+        if (data.status === 'OK') {
+            $('div#api_status').addClass('available');
+            console.log('todo ok')
+        }
+    });
+});
+
